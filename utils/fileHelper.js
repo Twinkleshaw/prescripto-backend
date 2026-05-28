7016a91381b6d03655398ace42f0e5881dd6166b
Twinkle Shaw <twinkleshaw68216@gmail.com>
2026-05-28 13:10:43 +0530

    added

--- utils/fileHelper.js
+++ utils/fileHelper.js
@@ -1,4 +1,5 @@
 export const getFileUrl = (req, folder, filename) => {
-    const baseUrl = `${req.protocol}://${req.get("host")}`;
-    return `${baseUrl}/${folder}/${filename}`;
+  const baseUrl =
+    process.env.BASE_URL || `${req.protocol}://${req.get("host")}`;
+  return `${baseUrl}/${folder}/${filename}`;
 };
